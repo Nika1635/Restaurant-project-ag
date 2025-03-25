3d55d53912cef21488589ae95631b830213b008b
nika robakidze <nika70252@gmail.com>
2025-03-25 16:44:26 +0400

    fullFilter

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,4 +10,12 @@ export class ApiService {
   getCards(){
     return this.api.get("https://restaurant.stepprojects.ge/api/Products/GetAll")
   }
+
+  getCategories(){
+    return this.api.get("https://restaurant.stepprojects.ge/api/Categories/GetAll")
+  }
+
+  filter(vegetarian: string, nuts: string, spiciness: string, categoriestring: string){
+    return this.api.get(`https://restaurant.stepprojects.ge/api/Products/GetFiltered?${vegetarian}${nuts}${spiciness}${categoriestring}`)
+  }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
@@ -12,14 +15,72 @@ export class ProductComponent implements OnInit {
 
   ngOnInit(): void {
     this.getcards()
+    this.getcategories()
   }
 
+  categories: any
   products: any
+  slider: number = -1
+  nuts: boolean = false
+  vegetarian: boolean = false
+  currentcategorieid: number = 0
+  
+
+  nutsstring: string = ``
+  vegetarianstring: string = ``
+  sliderstring: string = ``
+  categoriestring: string = ``
 
   getcards(){
     this.api.getCards().subscribe((data) => {
       this.products = data
-      console.log(data)
+    })
+    
+    this.nutsstring = ``
+    this.vegetarianstring = ``
+    this.sliderstring = ``
+    this.categoriestring = ``
+    this.currentcategorieid = 0
+  }
+
+  getcategories(){
+    this.api.getCategories().subscribe(data => {
+      this.categories = data
+    }) 
+  }
+
+  change(id: number){
+    this.currentcategorieid = id
+  }
+
+  filter(){
+    if(this.nuts == true){
+      this.nutsstring = `nuts=${this.nuts}&`
+    }else{
+      this.nutsstring = ``
+    }
+
+    if(this.vegetarian == true){
+      this.vegetarianstring = `vegeterian=${this.vegetarian}&`
+    }else{
+      this.vegetarianstring = ``
+    }
+    
+    if(this.slider > -1){
+      this.sliderstring = `spiciness=${this.slider}&`
+    }else{
+      this.sliderstring = ``
+    }
+
+    if(this.currentcategorieid > 0){
+      this.categoriestring = `categoryId=${this.currentcategorieid}&`
+    }else{
+      this.categoriestring = ``
+    }
+
+    
+    this.api.filter(this.vegetarianstring, this.nutsstring, this.sliderstring, this.categoriestring).subscribe(data => {
+      this.products = data
     })
   }
 }
